4ef58dfbd0c3bf449d2e641ec90e867904465c5c
florianvazelle <ponythugflorian@gmail.com>
2024-01-14 12:11:48 +0100

    feat: use esc to go back to menu

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,7 +1,5 @@
 extends Node3D
 
-@export var fast_close := true
-
 
 func _init():
 	if OS.is_debug_build():
@@ -15,19 +13,10 @@ func _ready() -> void:
 	get_viewport().scaling_3d_scale = 0.25
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-	if !OS.is_debug_build():
-		fast_close = false
-
-	if fast_close:
-		Log.debug("** Fast Close enabled in the 'main.gd' script **")
-		Log.debug("** 'Esc' to close 'Shift + F1' to release mouse **")
-
-	set_process_input(fast_close)
-
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"ui_cancel"):
-		get_tree().quit()  # Quits the game
+		get_tree().change_scene_to_file("res://scenes/menu.tscn")
 
 	if event.is_action_pressed(&"change_mouse_input"):
 		match Input.get_mouse_mode():
@@ -35,11 +24,3 @@ func _input(event: InputEvent) -> void:
 				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 			Input.MOUSE_MODE_VISIBLE:
 				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-
-# Capture mouse if clicked on the game, needed for HTML5
-# Called when an InputEvent hasn't been consumed by _input() or any GUI item
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
