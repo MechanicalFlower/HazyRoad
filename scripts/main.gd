47bb30cd9c05062719610a549767952a4148cbec
florianvazelle <ponythugflorian@gmail.com>
2024-02-10 11:38:45 +0100

    feat: add bootsplash (#25)
    * feat: add bootsplash
    
    * chore: add changelog entry

--- scripts/main.gd
+++ scripts/main.gd
@@ -12,11 +12,14 @@ func _init():
 func _ready() -> void:
 	get_viewport().scaling_3d_scale = 0.25
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	get_node(^"UI/PauseMenu").connect(
+		&"back_to_main_pressed", get_tree().change_scene_to_file.bind("res://scenes/menu.tscn")
+	)
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed(&"ui_cancel"):
-		get_tree().change_scene_to_file("res://scenes/menu.tscn")
+	# if event.is_action_pressed(&"ui_cancel"):
+	# 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
 
 	if event.is_action_pressed(&"change_mouse_input"):
 		match Input.get_mouse_mode():
